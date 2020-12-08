af7f171aa1112a813f18d59317119c0b382254ca
Josh Frank <eurosewusa@gmail.com>
2020-12-07 22:30:23 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?( "." )
   end
 
   def question?
-
+    self.end_with?( "?" )
   end
 
   def exclamation?
-
+    self.end_with?( "!" )
   end
 
   def count_sentences
-
+    resultRegex = Regexp.union( ".", "!", "?" )
+    resultArray = self.squeeze.split( resultRegex )
+    resultArray.reject { |s| s.empty? }
+    resultArray.length
   end
 end
+
+test = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+puts test.count_sentences
